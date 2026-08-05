e56933213ff722aad06c53f14e6e65d4450cbc76
0pyaq0 <0devbenny@gmail.com>
2026-08-05 18:04:47 +0900

    초기 커밋

---
+++ src/main/java/kopo/kkeudeok/KkeudeokApplication.java
@@ -0,0 +1,13 @@
+package kopo.kkeudeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkeudeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkeudeokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kopo/kkeudeok/KkeudeokApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.kkeudeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkeudeokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
